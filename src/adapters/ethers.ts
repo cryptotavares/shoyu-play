c4bcb1c153a80e8fd870239a21d13e79fef510fc
João Tavares <joaon_tavares@outlook.com>
2021-10-21 00:41:29 +0100

    add ethers configuratio to connect to rinkeby and kovan
    We are using infura to connect to both kovan and rinkeby

--- src/adapters/ethers.ts
+++ src/adapters/ethers.ts
@@ -1,10 +1,33 @@
-import { ContractInterface, ethers } from 'ethers';
+import { ContractInterface, ethers, providers } from 'ethers';
 import identityManangerAbi from '../abi/identityManagerAbi.json';
-import { PRIVATE_KEY, NETWORKS, SIGN_MESSAGE } from '../configs';
+import { PRIVATE_KEY, NETWORKS, SIGN_MESSAGE, INFURA_API_KEY, INFURA_PROJECT_ID } from '../configs';
 import logger from './logger';
 import StackedError from './StackedError';
 
 const ChainProviderMap = new Map<number, ethers.Contract>();
+
+const getContractFromProvider = (
+  provider: providers.Provider,
+  contractAddress: string,
+  privateKey: string,
+) => {
+  const wallet = new ethers.Wallet(privateKey, provider);
+  return new ethers.Contract(contractAddress, identityManangerAbi as ContractInterface, wallet);
+};
+
+const getNetworkConfig = (chainId: number) => {
+  const networkConfig = Object.values(NETWORKS).find(value => value.chainId === chainId);
+
+  if (!networkConfig) {
+    throw new StackedError('No contract address for given chainId');
+  }
+
+  if (!networkConfig.enabled) {
+    throw new StackedError('Network not enabled');
+  }
+
+  return networkConfig;
+};
 
 export const getContract = (chainId: number) => {
   const storedContract = ChainProviderMap.get(chainId);
@@ -13,19 +36,31 @@ export const getContract = (chainId: number) => {
     return storedContract;
   }
 
-  const contractAddress = Object.values(NETWORKS).find(value => value.chainId === chainId);
-
-  if (!contractAddress) {
-    throw new StackedError('No contract address for given chainId');
-  }
+  const contractAddress = getNetworkConfig(chainId);
 
   logger.debug('Creating new contract instance');
 
   switch (chainId) {
     case NETWORKS.default.chainId: {
-      const provider = new ethers.providers.JsonRpcProvider();
-      const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
-      const contract = new ethers.Contract(contractAddress.address, identityManangerAbi as ContractInterface, wallet);
+      const contract = getContractFromProvider(
+        new ethers.providers.JsonRpcProvider(),
+        contractAddress.address,
+        PRIVATE_KEY,
+      );
+
+      ChainProviderMap.set(chainId, contract);
+      return contract;
+    }
+    case NETWORKS.kovan.chainId:
+    case NETWORKS.rinkeby.chainId: {
+      const contract = getContractFromProvider(
+        new ethers.providers.InfuraProvider(
+          chainId,
+          { projectId: INFURA_PROJECT_ID, projectSecret: INFURA_API_KEY },
+        ),
+        contractAddress.address,
+        PRIVATE_KEY,
+      );
 
       ChainProviderMap.set(chainId, contract);
       return contract;
--- src/configs/index.ts
+++ src/configs/index.ts
@@ -16,20 +16,26 @@ export const SHUTDOWN_RETRY_INTERVAL = 500;
 export const PRIVATE_KEY = process.env.PRIVATE_KEY || '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
 export const SIGN_MESSAGE = process.env.SIGN_MESSAGE || "I'd like to sign in";
 
+export const INFURA_API_KEY = process.env.INFURA_API_KEY;
+export const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID;
+
 export const NETWORKS = {
   default: {
     chainId: process.env.DEFAULT_CHAIN_ID || 1337,
     address: process.env.DEFAULT_ADDRESS || '0x5fbdb2315678afecb367f032d93f642f64180aa3',
     blockDeployed: 1,
+    enabled: IS_DEVELOPMENT,
   },
   kovan: {
     chainId: 42,
-    address: process.env.KOVAN_ADDRESS || '0x5fbdb2315678afecb367f032d93f642f64180aa3',
-    blockDeployed: 1,
+    address: process.env.KOVAN_ADDRESS || '',
+    blockDeployed: parseInt(process.env.KOVAN_BLOCK_DEPLOY || '27841234', 10),
+    enabled: [true, 'true'].includes(process.env.KOVAN_ENABLED || false),
   },
   rinkeby: {
     chainId: 4,
-    address: process.env.RINKEBY_ADDRESS || '0x5fbdb2315678afecb367f032d93f642f64180aa3',
-    blockDeployed: 1,
+    address: process.env.RINKEBY_ADDRESS || '',
+    blockDeployed: parseInt(process.env.RINKEBY_BLOCK_DEPLOY || '9483224', 10),
+    enabled: [true, 'true'].includes(process.env.RINKEBY_ENABLED || false),
   },
 };
